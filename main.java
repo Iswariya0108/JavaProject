e48c384dc30dabdc5468dee145d509e01e37b6ed
Iswariya0108 <aiswariya0108@gmail.com>
2024-04-03 15:29:32 +0530

    Added variable b

--- main.java
+++ main.java
@@ -1,10 +1,11 @@
 public class Main
 {
-   static int a = 10, int b = 20;
-
   public static void main(String args[])
   {
-    System.out.println(a,b);
+     int a = 10;
+     int b = 20;
+    System.out.println(a);
+    System.out.println(b);
     
     
   }
